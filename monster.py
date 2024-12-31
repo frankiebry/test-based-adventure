6c964371dda3ffd9495c13c8a1dba34890d3b012
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-31 15:26:16 -0500

    Add monster.avoid_player() function

--- monster.py
+++ monster.py
@@ -12,24 +12,33 @@ class Monster:
         initial_position (tuple): The (x, y) coordinates of the monster's starting position.
         """
         self.position = initial_position
-        self.turns_since_move = 0  # Track how many turns since the last move for the cooldown timer
         self.repellent_turns_left = 0  # Track how many turns the repellent is active
 
     def random_move(self):
         """
         Moves the monster in a random direction (north, south, east, west) if the move is within bounds.
+        If the monster tries to move out of bounds, another random direction is chosen.
+        20% chance that the monster will not move.
         """
-        directions = ["north", "south", "east", "west"]
-        direction = random.choice(directions)
-        
-        if direction == "north" and self.position[1] > 0:
-            self.position = (self.position[0], self.position[1] - 1)
-        elif direction == "south" and self.position[1] < settings.GRID_HEIGHT - 1:
-            self.position = (self.position[0], self.position[1] + 1)
-        elif direction == "east" and self.position[0] < settings.GRID_WIDTH - 1:
-            self.position = (self.position[0] + 1, self.position[1])
-        elif direction == "west" and self.position[0] > 0:
-            self.position = (self.position[0] - 1, self.position[1])
+        directions = ["north", "south", "east", "west", "yawn"]
+        while True:  # Loop until a valid move is made
+            direction = random.choice(directions)
+            
+            if direction == "north" and self.position[1] > 0:
+                self.position = (self.position[0], self.position[1] - 1)
+                break
+            elif direction == "south" and self.position[1] < settings.GRID_HEIGHT - 1:
+                self.position = (self.position[0], self.position[1] + 1)
+                break
+            elif direction == "east" and self.position[0] < settings.GRID_WIDTH - 1:
+                self.position = (self.position[0] + 1, self.position[1])
+                break
+            elif direction == "west" and self.position[0] > 0:
+                self.position = (self.position[0] - 1, self.position[1])
+                break
+            elif direction == "yawn":
+                typewriter("You hear a low yawn echoing in the distance", 0.05)
+                break
 
     def is_near_player(self, player_position):
         """
@@ -53,13 +62,31 @@ class Monster:
         player_position (tuple): The (x, y) coordinates of the player's position.
         """
         # Move towards the player horizontally or vertically
-        if self.position[0] < player_position[0]:
+        if self.position[0] < player_position[0] and self.position[0] < settings.GRID_WIDTH - 1:
             self.position = (self.position[0] + 1, self.position[1])
-        elif self.position[0] > player_position[0]:
+        elif self.position[0] > player_position[0] and self.position[0] > 0:
             self.position = (self.position[0] - 1, self.position[1])
-        elif self.position[1] < player_position[1]:
+        elif self.position[1] < player_position[1] and self.position[1] < settings.GRID_HEIGHT - 1:
             self.position = (self.position[0], self.position[1] + 1)
-        elif self.position[1] > player_position[1]:
+        elif self.position[1] > player_position[1] and self.position[1] > 0:
+            self.position = (self.position[0], self.position[1] - 1)
+
+    # This is basically just the opposite of chase_player(). Can this code be less repetitive?
+    def avoid_player(self, player_position):
+        """
+        Moves the monster one step away from the player's position.
+        
+        Args:
+        player_position (tuple): The (x, y) coordinates of the player's position.
+        """
+        # Move away from the player horizontally or vertically
+        if self.position[0] > player_position[0] and self.position[0] < settings.GRID_WIDTH - 1:
+            self.position = (self.position[0] + 1, self.position[1])
+        elif self.position[0] < player_position[0] and self.position[0] > 0:
+            self.position = (self.position[0] - 1, self.position[1])
+        elif self.position[1] > player_position[1] and self.position[1] < settings.GRID_HEIGHT - 1:
+            self.position = (self.position[0], self.position[1] + 1)
+        elif self.position[1] < player_position[1] and self.position[1] > 0:
             self.position = (self.position[0], self.position[1] - 1)
 
     def check_if_caught(self, player_position):
@@ -72,9 +99,9 @@ class Monster:
         Returns:
         bool: True if the monster has caught the player, otherwise False.
         """
-        if self.position == player_position:
-            return True
-        return False
+        if self.repellent_turns_left > 0:
+            return False
+        return self.position == player_position
     
     def move(self, player_position):
         """
@@ -85,18 +112,15 @@ class Monster:
         player_position (tuple): The (x, y) coordinates of the player's position.
         """
         
-        # TODO write this more elegantly
-        self.turns_since_move += 1
-        if self.turns_since_move >= 2:
-            self.turns_since_move = 0
-            # If repellent is active, move randomly for 3 turns
-            if self.repellent_turns_left > 0:
-                self.repellent_turns_left -= 1
-                self.random_move()
-            # If the monster is close to the player, it chases the player,
-            elif self.is_near_player(player_position):
-                typewriter("\033[91mYou hear a bloodcurdling howl as a foul stench fills the air.\033[0m", 0.05)
-                self.chase_player(player_position)
-            # else it moves randomly
-            else:
-                self.random_move()
+        # If repellent is active, the monster avoids the player
+        if self.repellent_turns_left > 0:
+            self.repellent_turns_left -= 1
+            typewriter("\033[95mYou hear a disgruntled growl as the sound of heavy footfalls fade away.\033[0m", 0.05)
+            self.avoid_player(player_position)
+        # If the monster is close to the player and no repellent is active, it chases the player,
+        elif self.is_near_player(player_position):
+            typewriter("\033[91mYou hear a bloodcurdling howl as a foul stench fills the air.\033[0m", 0.05)
+            self.chase_player(player_position)
+        # else it moves randomly
+        else:
+            self.random_move()
