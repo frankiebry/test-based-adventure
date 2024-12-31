c61e5cfdd416ecdd5e4921cc09c87b7a449187d1
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-31 11:52:15 -0500

    Add find_random_item() function

--- inventory.py
+++ inventory.py
@@ -1,3 +1,6 @@
+import random
+from typewriter import typewriter
+
 class Inventory:
     def __init__(self):
         """Initialize the player's inventory with default items."""
@@ -12,6 +15,28 @@ class Inventory:
             self.items[item] += count
         else:
             self.items[item] = count
+
+    def find_random_item(self):
+        """Roll virtual dice and add items to player inventory based on luck."""
+        roll = random.randint(1, 20)  # Simulate a D20 roll (integer between 1 and 20)
+    
+        if 1 <= roll <= 5:  # Nothing: 25% chance
+            typewriter("There is nothing here.", 0.05)
+        elif 6 <= roll <= 10: # Torch: 25% chance
+            self.add_item("torch", 1)
+            typewriter("You found a torch!", 0.05)
+        elif 11 <= roll <= 15:  # Monster Repellent: 25% chance
+            self.add_item("monster repellent", 1)
+            typewriter("You found some \033[95mmonster repellent\033[0m!", 0.05)
+        elif 16 <= roll <= 17:  # Ruby: 7.5% chance
+            self.add_item("ruby", 1)
+            typewriter("You found a \033[91mruby\033[0m!", 0.05)
+        elif 18 <= roll <= 19:  # Emerald: 7.5% chance
+            self.add_item("emerald", 1)
+            typewriter("You found an \033[92memerald\033[0m!", 0.05)
+        elif roll == 20:  # Diamond: 5% chance
+            self.add_item("diamond", 1)
+            typewriter("You found a \033[96mdiamond\033[0m!", 0.05)
 
     def use_item(self, item):
         """Use an item from the inventory, if available."""
--- main.py
+++ main.py
@@ -102,12 +102,8 @@ class Game:
                 self.key_position = None  # Remove the key from the map
                 typewriter("You found the \033[93mkey\033[0m!", 0.05)
                 print(' ')
-            # Generate a random number between 0 and 1
-            elif random.random() < 0.1: # 10% chance
-                inventory.add_item("torch", 1) # Add a torch to the inventory
-                typewriter("You found a torch!", 0.05)
             else:
-                typewriter("There is nothing here.", 0.05)
+                inventory.find_random_item()
 
     def display_commands(self):
         """Display the list of available commands to the player."""
